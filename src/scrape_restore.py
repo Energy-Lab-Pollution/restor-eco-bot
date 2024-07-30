700b8b79432663762b65963dfed55d4a78c63884
Federico Domínguez Molina <fd.molina@outlook.com>
2024-07-30 13:57:31 -0500

    Trying to make API work

--- src/scrape_restore.py
+++ src/scrape_restore.py
@@ -1,3 +1,18 @@
 """
 Script that contains the code to scrape the Restor-Eco page
 """
+
+import requests
+
+from util.constants import API, HEADERS
+
+
+api = API.replace("x", "1").replace("y", "30")
+path = api.split("/")
+path = path
+
+
+print(api)
+print(path)
+r = requests.post(api, headers=HEADERS)
+print(r.content)
--- src/util/constants.py
+++ src/util/constants.py
@@ -2,22 +2,26 @@
 Script with constaants used throughout the project
 """
 
-API = "https://restor2-prod-1-api.restor.eco/search/1/organizations/search?page=x&size=y&sort=isFeatured%3Adesc"
+API = "https://restor2-prod-1-api.restor.eco/search/1/organizations/search?page=x&size=y"
 
 HEADERS = {
-    "Accept": "application/json, text/plain, */*",
-    "Accept-encoding": "gzip, deflate, br, zstd",
-    "Accept-language": "en-US,en;q=0.9",
-    "Content-length": 2,
-    "Content-type": "application/json",
-    "Origin": "https://restor.eco",
-    "Priority": "u=1, i",
-    "Referer": "https://restor.eco/",
-    "Sec-ch-ua": '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
-    "Sec-ch-ua-mobile": "?0",
-    "Sec-ch-ua-platform": "Windows",
-    "Sec-fetch-dest": "empty",
-    "Sec-fetch-mode": "cors",
-    "Sec-fetch-site": "same-site",
-    "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+    "authority": "restor2-prod-1-api.restor.eco",
+    "method": "POST",
+    "path": "search/1/organizations/search?page=x&size=y",
+    "scheme": "htpps",
+    "accept": "application/json, text/plain, */*",
+    "accept-encoding": "gzip, deflate, br, zstd",
+    "accept-language": "en-US,en;q=0.9",
+    "content-length": "2",
+    "content-type": "application/json",
+    "origin": "https://restor.eco",
+    "priority": "u=1, i",
+    "referer": "https://restor.eco/",
+    "sec-ch-ua": '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "Windows",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
 }

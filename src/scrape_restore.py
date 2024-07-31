1104f32f4b1d8ad8ebf00ad4a138409b9712284c
Federico Domínguez Molina <fd.molina@outlook.com>
2024-07-31 14:04:03 -0500

    Added method to extract the organization's information

--- src/scrape_restore.py
+++ src/scrape_restore.py
@@ -6,7 +6,9 @@ import os
 import time
 
 from util.constants import URL
+from bs4 import BeautifulSoup
 from util.driver_manager import DriverManager
+
 
 current_dir = os.getcwd()
 
@@ -56,10 +58,47 @@ class RestoreEcoScraper:
 
     def hover_to_org(self):
         """
-        Hovers to the first org in the page
+        Hovers to a given org in the webpage
         """
-        xpath = "/html/body/div[1]/div/div[2]/aside/div/div/div/div/div[2]/div/ul/li[1]/a/article/div/span[2]"
+        xpath = "/html/body/div[1]/div/div[2]/aside/div/div/div/div/div[2]/div/ul/li[10]/a/article/div/span[2]"
         self.chrome_driver.hover_by_xpath(xpath)
+
+    def extract_available_orgs(self):
+        """
+        This function extracts the html from the webpage
+        """
+        html = self.chrome_driver.driver.page_source
+        soup = BeautifulSoup(html, "html.parser")
+        li_elements = soup.find_all("li")
+
+        dicts_list = []
+
+        for li_element in li_elements:
+            a_tag = li_element.find("a")
+            tmp_dict = {}
+            if a_tag and "href" in a_tag.attrs:
+                # Get the 'href' attribute
+                href = a_tag["href"]
+                org_name = li_element.find(
+                    "span", {"class": "paragraph-md-bold"}
+                )
+
+                img_span = li_element.find(
+                    "span", {"class": "w-12 pt-[0.25rem]"}
+                )
+
+                tmp_dict["org_name"] = org_name.text
+                tmp_dict["url"] = f"https://restor.eco{href}"
+                tmp_dict["country_name"] = None
+
+                if img_span:
+                    img = img_span.find("img")
+                    raw_country_name = img["alt"]
+                    tmp_dict["country_name"] = raw_country_name
+
+                dicts_list.append(tmp_dict)
+
+        print(dicts_list)
 
 
 if __name__ == "__main__":
@@ -69,4 +108,4 @@ if __name__ == "__main__":
     time.sleep(3)
     restore_scraper.hover_to_org()
     time.sleep(2)
-    restore_scraper.chrome_driver.scroll_down()
+    restore_scraper.extract_available_orgs()
